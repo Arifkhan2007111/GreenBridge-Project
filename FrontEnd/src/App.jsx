6f5a8a12b5dd6e2913a6509117e63af774551a39
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-03-15 19:15:28 +0530

    First Commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+import Landing from './Components/landingPage'
+
+function App() {
+
+  return (
+    <>
+      <Landing />
+    </>
+  )
+}
+
+export default App
---
+++ FrontEnd/src/Components/LandingPage.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Landing = () =>{
+    return(
+        <>
+            <div className="Main">
+                <nav>
+                    <div className="logo">
+                        <h1>LOGO</h1>
+                    </div>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>Awareness</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Connect</a></li>
+                    </ul>
+                </nav>
+
+                <section className="Home">
+                    <div className="home-cover">
+
+                    </div>
+                </section>
+                <section className="Awareness">
+
+                </section>
+                <section className="About">
+
+                </section>
+                <section className="Connect">
+
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Landing
