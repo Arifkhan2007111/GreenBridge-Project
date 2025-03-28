f8b67f26ba82f9bd824db5d8cb381174547cd7c7
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-03-28 23:22:21 +0530

    Some Changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import './App.css'
+import Lenis from 'lenis'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Landing from './Components/landingPage'
+import Landing from './Components/LandingPage'
+import Login from './Components/Login'
 
 function App() {
+  // Initialize Lenis
+    const lenis = new Lenis({
+        duration: 1.3
+    });
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
 
   return (
     <>
-      <Landing />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Landing />}></Route>
+          <Route exact path='/login' element={<Login />}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
