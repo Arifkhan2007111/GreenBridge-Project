e54326636f59addf2e1ead44b405fbf51e45ce3f
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-04-03 23:56:36 +0530

    Some changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Landing from './Components/LandingPage'
 import Login from './Components/Login'
+import Blog from './Components/Bloging'
 
 function App() {
   // Initialize Lenis
@@ -26,6 +27,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Landing />}></Route>
           <Route exact path='/login' element={<Login />}></Route>
+          <Route exact path='/blog' element={<Blog />}></Route>
         </Routes>
       </Router>
     </>
