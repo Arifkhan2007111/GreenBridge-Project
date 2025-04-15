e24b558deccb7f09c08eded16fe5e9e10772dece
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-04-15 12:07:51 +0530

    Some changes

--- FrontEnd/src/Components/Login.jsx
+++ FrontEnd/src/Components/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { app } from "../firebase.js"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 
-const auth = getAuth(app)
 
 const Login = () =>{
+    const auth = getAuth(app)
+    const googleProvider = new GoogleAuthProvider();
+
 
     const [email, setEmail] = useState("")
     const [password, SetPassword] = useState("")
@@ -20,14 +22,22 @@ const Login = () =>{
             console.error("Error:", error.message);
         });
     }
+
+    const googleSignup = () =>{
+        signInWithPopup(auth, googleProvider);
+
+    }
     return(
         <>
             <div className="login">
+                <h1>Login</h1>
                 <form>
                     <input onChange={(e) =>{setEmail(e.target.value)}} value={email} placeholder="Enter Your Email" type="email"></input>
                     <input onChange={(e) =>{SetPassword(e.target.value)}} value={password} placeholder="Enter Your Password" type="password"></input>
-                    <button onClick={signup}>Submit</button>
+                    <button className="button" onClick={signup}>Submit</button>
                 </form>
+                <p>or</p>
+                <button onClick={googleSignup} className="button">Continue with Google</button>
             </div>
         </>
     )
